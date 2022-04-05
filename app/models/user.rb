4e744aaab95b0567833e27bec93750a213de3d21
pro-soichiro <dr.1987626@gmail.com>
2022-04-05 02:58:16 +0000

    独自のメソッドによるバリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  # Userモデルの属性emailが適正なメールアドレスかを
+  # 独自のメソッドによるバリデーションによって実装する
+  validate :email_check
+
+  private
+  def email_check
+    email_pattern = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+    unless email_pattern.match(self.email)
+      errors.add(:email, "正しいメールアドレスを入力してください")
+    end
+  end
+
 end
