0cbad878433dd7cd658a8fef3b71c783e01d9629
pro-soichiro <dr.1987626@gmail.com>
2022-04-18 14:05:52 +0000

    Userコントローラーとビューの設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show,:edit,:update,:destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name,:address,:email,:birthday)
+    end
+end
