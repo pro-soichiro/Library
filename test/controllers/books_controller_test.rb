1cf46d19aa256f0593b9feda161740c463f24e99
pro-soichiro <dr.1987626@gmail.com>
2022-05-02 08:40:44 +0000

    機能テスト

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -2,47 +2,27 @@ require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @book = books(:one)
+    @book = books(:book_1)
+    upload_file = Rack::Test::UploadedFile.new(Rails.root.join("test/fixtures/files/yoakemae.jpg"))
+    @book_images = [upload_file]
   end
 
-  test "should get index" do
-    get books_url
-    assert_response :success
-  end
-
-  test "should get new" do
+  test "新規図書入力機能の呼び出しと更新" do
+    # 新規図書入力画面の呼び出し評価
     get new_book_url
     assert_response :success
-  end
-
-  test "should create book" do
+    assert_select 'h1',"New Book"
+    # 新規図書登録処理の評価（図書件数の差分評価とリダイレクト評価）
     assert_difference('Book.count') do
-      post books_url, params: { book: { description: @book.description, title: @book.title } }
+      post books_url, params:{book: {title: "夜明け前", description: "幕末から明治の変化を描く", images: @book_images}}
     end
-
-    assert_redirected_to book_url(Book.last)
+    # 新規図書登録後のリダイレクトを確認
+    assert_response :redirect
   end
 
-  test "should show book" do
+  test "該当図書の表示" do
     get book_url(@book)
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_book_url(@book)
-    assert_response :success
-  end
-
-  test "should update book" do
-    patch book_url(@book), params: { book: { description: @book.description, title: @book.title } }
-    assert_redirected_to book_url(@book)
-  end
-
-  test "should destroy book" do
-    assert_difference('Book.count', -1) do
-      delete book_url(@book)
-    end
-
-    assert_redirected_to books_url
-  end
 end
